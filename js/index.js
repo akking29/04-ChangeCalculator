b2cec084e63b859dfb861f9b9808a6281b81c24e
akking29 <akking@live.com>
2016-04-22 19:58:28 -0700

    Added files via upload

--- js/index.js
+++ js/index.js
@@ -1,20 +1,37 @@
 var change = function(){
 	//set change variables
 	
-	var total = parseFloat($('#total').val());
+	var total = $('#total').val();
 	//get user input "total"
-	total = total.toFixed(2);
 	console.log(total);
 	//test
-	var received = parseFloat($('#received').val());
+	var received = $('#received').val();
 	//get user input "received"
-	received = received.toFixed(2);
 	console.log(received);
 	//test
+	//get answer to break into change
 	var answer = received - total;
-	answer = answer.toFixed(2);
 
-	$('#answer').html('$' + answer + ' is your change.'); 
+	var dollar = Math.floor((answer * 100)/100);
 
-}
+	var remaining = (answer * 100)% 100;
+	var quarter = Math.floor(remaining /25);
+
+	remaining = remaining % 25;
+	var dime = Math.floor(remaining /10);
+
+	remaining = remaining % 10;
+	var nickel = Math.floor(remaining /5);
+
+	remaining = remaining % 5;
+	var penny = Math.round(remaining /1);
+
+
+	//print message to screen
+	$('#d').html('Your change is ' + dollar + ' dollar(s)')
+	$('#c').html(' and ' + quarter + dime + nickel + penny) 
+} 
+
+
+
 
